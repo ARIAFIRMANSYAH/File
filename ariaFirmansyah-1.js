0787a73f97ed5ab9c6407e6f3aaad25c1ec2d6f3
ARIAFIRMANSYAH <114503318+ARIAFIRMANSYAH@users.noreply.github.com>
2023-10-05 21:25:31 +0700

    Add files via upload

---
+++ ariaFirmansyah-1.js
@@ -0,0 +1,10 @@
+let fisrtName = 'Aria'
+let lastName = 'Firmansyah'
+let age = 19
+let isMaried = 'false'
+
+
+console.log(fisrtName)
+console.log(lastName)
+console.log(age)
+console.log(isMaried)
